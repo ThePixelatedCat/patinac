fb4d4a864e1d0ccdd7a96edf5700c823e01a91b6
ThePixelatedCat <acfrostie@gmail.com>
2026-02-03 16:38:37 +1100

    Improved script

--- coverage.ps1
+++ coverage.ps1
@@ -1,6 +1,5 @@
-$env:RUSTFLAGS='-Cinstrument-coverage'; 
 $env:LLVM_PROFILE_FILE='cargo-test.profraw'; 
-cargo test --config build.incremental=false;
+cargo test --config build.incremental=false --config build.rustflags=['\"-Cinstrument-coverage\"'];
 
 grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html;
 Remove-Item C:\Users\acfro\Documents\Programming\Projects\Languages\patina\cargo-test.profraw;
