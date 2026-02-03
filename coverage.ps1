99548f99c5f3f5ebf1231b5dcee487b5c53fd641
ThePixelatedCat <acfrostie@gmail.com>
2026-02-03 16:51:37 +1100

    finalised??? script

--- coverage.ps1
+++ coverage.ps1
@@ -1,7 +1,7 @@
 $env:LLVM_PROFILE_FILE='cargo-test.profraw'; 
 cargo test --config build.incremental=false --config build.rustflags=['\"-Cinstrument-coverage\"'];
 
-grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" -o target/coverage/html;
+grcov . --binary-path ./target/debug/deps/ -s . -t html --branch --ignore-not-existing --ignore '../*' --ignore "/*" --ignore "*/main.rs" -o target/coverage/;
 Remove-Item C:\Users\acfro\Documents\Programming\Projects\Languages\patina\cargo-test.profraw;
 Remove-Item C:\Users\acfro\Documents\Programming\Projects\Languages\patina\compiler\cargo-test.profraw;
 Remove-Item C:\Users\acfro\Documents\Programming\Projects\Languages\patina\runtime\cargo-test.profraw;
